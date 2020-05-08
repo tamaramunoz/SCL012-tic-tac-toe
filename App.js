d817d73259305ee01a2137195b82d2affc547a84
tamaramunoz <tami.skt@gmail.com>
2020-05-08 13:28:56 -0400

    showing icons and giving position

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { MaterialCommunityIcons as Icon } from 'react-native-vector-icons'
 
 export default function App() {
 
-  const [game, setGame] = useState([])
+  const [game, setGame] = useState([
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+  ])
+
   const [playerOne, setPlayerOne] = useState(1)
   const [winner, setWinner] = useState("")
 
@@ -15,33 +20,62 @@ export default function App() {
       [0, 0, 0],
       [0, 0, 0],
     ]);
+
+    setWinner(null)
   }
+
+  const touchIcon = (row, col) => {
+    let value = game[row][col]
+
+    switch(value){
+      case 1: return <Icon name="close" style={styles.tileX} />
+      case -1: return <Icon name="circle-outline" style={styles.tileO} />
+      default: return <View />
+    }
+
+
+
+  };
 
   return (
     <View style={styles.container}>
 
       <View style={ {flexDirection: "row"} }>
         <View style={[styles.tile, { borderLeftWidth: 0, borderTopWidth: 0 }]} > 
-          <Icon name="close" style={styles.tileX}></Icon>
+          {touchIcon(0, 0)}
         </View>
 
         <View style={[styles.tile, { borderTopWidth: 0 }]} > 
-          <Icon name="circle-outline" style={styles.tileO} ></Icon>
+          {touchIcon(0, 1)}
         </View>
 
-        <View style={[styles.tile, { borderTopWidth: 0, borderRightWidth: 0 }]} />
+        <View style={[styles.tile, { borderTopWidth: 0, borderRightWidth: 0 }]} >
+          {touchIcon(0, 2)}
+        </View>
       </View>
 
       <View style={{flexDirection: 'row'}}>
-        <View style={[styles.tile, { borderLeftWidth: 0 }]} />
-        <View style={[styles.tile, {  }]} />
-        <View style={[styles.tile, { borderRightWidth: 0 }]} />
+        <View style={[styles.tile, { borderLeftWidth: 0 }]} >
+          {touchIcon(1, 0)}
+        </View>
+        <View style={[styles.tile, {  }]} >
+          {touchIcon(1, 1)}
+        </View>
+        <View style={[styles.tile, { borderRightWidth: 0 }]} >
+          {touchIcon(1, 2)}
+        </View>
       </View>
 
       <View style={{flexDirection: 'row'}}>
-        <View style={[styles.tile, { borderBottomWidth: 0, borderLeftWidth: 0 }]} />
-        <View style={[styles.tile, { borderBottomWidth: 0 }]} />
-        <View style={[styles.tile, { borderBottomWidth: 0, borderRightWidth: 0 }]} />
+        <View style={[styles.tile, { borderBottomWidth: 0, borderLeftWidth: 0 }]} >
+          {touchIcon(2, 0)}
+        </View>
+        <View style={[styles.tile, { borderBottomWidth: 0 }]} >
+          {touchIcon(2, 1)}
+        </View>
+        <View style={[styles.tile, { borderBottomWidth: 0, borderRightWidth: 0 }]} >
+          {touchIcon(2, 2)}
+        </View>
       </View>
 
     </View>
@@ -65,16 +99,12 @@ const styles = StyleSheet.create({
   tileX: {
     color: "red",
     fontSize: 60,
-    alignItems: "center",
-    justifyContent: "center"
   },
 
   tileO: {
     color: "green",
     fontSize: 60,
-    alignItems: "center",
-    justifyContent: "center"
+  },
 
-  }
 
 });
